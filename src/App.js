fcac905c3c943428ffab49221cfc1346b98cb444
Jyothirmai <jyothirmai.kakumanu2711@gmail.com>
2021-05-21 19:04:41 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [rowCount, setRowCount] = useState(10);
+  const [colCount, setColCount] = useState(10);
+  const [input, setInput] = useState('');
+
+  const arr = [];
+  const addInput = (e, colNum) => {
+    console.log("in line 11");
+    return (
+      // arr.forEach((row) => {
+        setInput(e.target.value);
+        // {target.id===colNum ? setInput(e.target.value): null}
+    )
+
+  }
+
+  const col = () => {
+    const colArr=[];
+    for (let i = 0; i < colCount; i++) {
+      console.log("col",i);
+      colArr.push(<td id={i} value={input} className="grid-col">
+        <input className="cell-input" onChange={(e) => addInput(e.target.id)}
+          value={input} />
+      </td>)
+    }
+    return colArr;
+  }
+  const row = () => {
+    for (let i = 0; i < rowCount; i++) {
+      console.log("row",i);
+      arr.push(<tr id={i} className="grid-row">
+        {col()}
+      </tr>)
+    }
+    return arr;
+  }
+
+  const createTable = () => {
+    return (
+      row()
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid">
+        <table className="table">
+          <tbody>
+            {createTable()}
+          </tbody>
+        </table>
+
+      </div>
     </div>
   );
 }
